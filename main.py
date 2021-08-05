acaa10dfc4fdfc06620663b3f634cf480cc482fa
Chun Hin D Tang <dtang955472@gmail.com>
2021-08-05 03:38:06 +0000

    makeArrayConsecutive2 algo added

--- main.py
+++ main.py
@@ -33,3 +33,10 @@ print(shapeArea(5))
 
 ###########################################
 
+def makeArrayConsecutive2(statues):
+  return max(statues) - min(statues) - len(statues) + 1
+
+print(makeArrayConsecutive2([6, 2, 3, 8]))
+
+###########################################
+
