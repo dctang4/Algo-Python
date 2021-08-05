e4c560f271e9abc9c4575259456ce340256e4785
Chun Hin D Tang <dtang955472@gmail.com>
2021-08-05 03:33:08 +0000

    adjacentElementsProduct algo added

--- main.py
+++ main.py
@@ -18,3 +18,10 @@ def checkPalindrome(inputString):
 print(checkPalindrome('noon'))
 
 ###########################################
+
+def adjacentElementsProduct(inputArray):
+  return max([inputArray[i]*inputArray[i+1] for i in range(len(inputArray)-1)])
+
+print(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
+
+###########################################
