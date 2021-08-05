943a707c448ed1524171bcea2ffff700a42bb34c
Chun Hin D Tang <dtang955472@gmail.com>
2021-08-05 03:48:49 +0000

    matrixElementsSum algo added

--- main.py
+++ main.py
@@ -77,3 +77,20 @@ print(almostIncreasingSequence([10, 1, 2, 3, 4, 5]))
 
 ###########################################
 
+# After becoming famous, the CodeBots decided to move into a new building together. Each of the rooms has a different cost, and some of them are free, but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+
+# Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, your task is to return the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that don't appear below a 0).
+
+def matrixElementsSum(matrix):
+  count = 0
+  for j in range(len(matrix[0])):
+    for i in range(len(matrix)):
+      if matrix[i][j] == 0:
+        break
+      else:
+        count += matrix[i][j]
+  return count
+
+print(matrixElementsSum([ [0, 1, 1, 2], 
+                          [0, 5, 0, 0], 
+                          [2, 0, 3, 3] ]))
