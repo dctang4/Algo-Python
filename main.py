fabeed032965f92e0071b0341b9972b99c79723c
Chun Hin D Tang <dtang955472@gmail.com>
2021-08-05 03:30:11 +0000

    checkPalindrome algo added

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def add(param1, param2):
-    return param1 + param2
+  return param1 + param2
 
 print(add(2,5))
 
+###########################################
+
 def centuryFromYear(year):
-    return (year+99)//100
+  return (year+99)//100
 
 print(centuryFromYear(1809))
 
+###########################################
+
+def checkPalindrome(inputString):
+  return inputString == inputString[::-1]
+
+print(checkPalindrome('noon'))
+
+###########################################
